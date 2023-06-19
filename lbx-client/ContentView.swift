f8274c673d55f1f3dc5dab76a16b893a89cef5dc
Ambrose Mbayi <91916741+mbayi-ios@users.noreply.github.com>
2023-06-19 18:32:05 +0300

    Initial Commit

---
+++ lbx-client/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  lbx-client
+//
+//  Created by Ambrose Mbayi on 19/06/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ lbx-client/lbx_clientApp.swift
@@ -0,0 +1,17 @@
+//
+//  lbx_clientApp.swift
+//  lbx-client
+//
+//  Created by Ambrose Mbayi on 19/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct lbx_clientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
